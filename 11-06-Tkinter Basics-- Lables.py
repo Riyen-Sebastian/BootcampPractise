dfb29bc19377441752935abefa57d363405c9a5b
Riyen-Sebastian <riyensebastian@gmail.com>
2024-06-11 12:57:51 +0530

    hello world program using labels in tkinter

---
+++ 11-06-Tkinter Basics-- Lables.py
@@ -0,0 +1,9 @@
+# Python tkinter hello world program using labels
+
+from tkinter import *
+
+root = Tk() 
+a = Label(root, text ="Hello World", bg="lightblue", font=("Arial", 16, "bold")) 
+a.pack() 
+
+root.mainloop() 
